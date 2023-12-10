e44c9ac95812bf49d95db8b9eee1839118e23093
Rümeysa B <102685123+beslirum@users.noreply.github.com>
2023-12-10 16:06:18 +0300

    Update breadth_first_search.py

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -1,14 +1,15 @@
 from collections import deque
 
 def breadth_first_search(graph, start):
-    visited = set()
-    queue = deque([start])
+    visited = set()  # Ziyaret edilen düğümleri tutmak için küme
+    queue = deque([start])  # Genişlik öncelikli aramada kullanılacak kuyruk
 
     while queue:
-        current_node = queue.popleft()
+        current_node = queue.popleft()  # Kuyruğun başındaki düğümü al
         if current_node not in visited:
             print(current_node)  # Düğümü ziyaret et
-            visited.add(current_node)
+            visited.add(current_node)  # Ziyaret edildi olarak işaretle
+            # Düğümün komşularını kuyruğa ekle (zaten ziyaret edilmemiş olanlar)
             queue.extend(neighbor for neighbor in graph[current_node] if neighbor not in visited)
 
 # Örnek bir graf yapısı
@@ -26,4 +27,3 @@ start_node = 'A'
 
 # Genişlik öncelikli aramayı başlat
 breadth_first_search(graph, start_node)
-
