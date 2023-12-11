033dfc9a6244062c2b693d6ed283dcf05001dae3
Rümeysa B <102685123+beslirum@users.noreply.github.com>
2023-12-11 13:51:23 +0300

    Update uniform_cost_search_EXAMPLE.py

--- uniform_cost_search_EXAMPLE.py
+++ uniform_cost_search_EXAMPLE.py
@@ -1,1 +1,79 @@
 
+import heapq
+
+class Node:
+    def __init__(self, state, path_cost):
+        self.state = state
+        self.path_cost = path_cost
+
+    def __lt__(self, other):
+        return self.path_cost < other.path_cost
+
+def uniform_cost_search(problem):
+    # Başlangıç düğümünü oluştur
+    initial_node = Node(state=problem.initial_state, path_cost=0)
+    
+    # Öncelik kuyruğunu başlangıç düğümüyle başlat
+    frontier = [initial_node]
+    
+    # Keşfedilen durumları tutan küme
+    explored = set()
+    
+    while frontier:
+        # Öncelik kuyruğundan en düşük maliyetli düğümü çıkar
+        current_node = heapq.heappop(frontier)
+        
+        # Hedef durumu kontrol et
+        if problem.goal_test(current_node.state):
+            return current_node.state  # Çözümü döndür
+        
+        # Keşfedilen durumlar kümesine ekle
+        explored.add(current_node.state)
+        
+        # Her bir aksiyon için
+        for action in problem.actions(current_node.state):
+            # Yeni çocuk düğümü oluştur
+            child = Node(state=problem.result(current_node.state, action),
+                         path_cost=current_node.path_cost + problem.step_cost(current_node.state, action))
+            
+            # Eğer çocuk düğümü keşfedilmemiş ve öncelik kuyruğunda yoksa
+            if child.state not in explored and child not in frontier:
+                heapq.heappush(frontier, child)  # Çocuk düğümü öncelik kuyruğuna ekle
+            elif child in frontier:
+                # Eğer çocuk düğümü öncelik kuyruğunda varsa ve daha düşük maliyetli ise
+                existing_node_index = frontier.index(child)
+                existing_node = frontier[existing_node_index]
+                if child.path_cost < existing_node.path_cost:
+                    # Öncelik kuyruğundaki düğümü çocuk düğümüyle değiştir
+                    frontier[existing_node_index] = child
+
+    return None  # Hedef duruma ulaşılamadı
+
+# Örnek bir problem sınıfı
+class Problem:
+    def __init__(self, initial_state, goal_state):
+        self.initial_state = initial_state
+        self.goal_state = goal_state
+
+    def goal_test(self, state):
+        return state == self.goal_state
+
+    def actions(self, state):
+        # Durumunuzda yapılabilecek aksiyonları döndürmek için uygulamanız gereken mantığı ekleyin
+        pass
+
+    def result(self, state, action):
+        # Bir aksiyonun uygulanmasının sonucunu döndürmek için uygulamanız gereken mantığı ekleyin
+        pass
+
+    def step_cost(self, state, action):
+        # Bir aksiyonun maliyetini döndürmek için uygulamanız gereken mantığı ekleyin
+        pass
+
+# Kullanım örneği
+initial_state = "A"
+goal_state = "C"
+problem_instance = Problem(initial_state, goal_state)
+solution = uniform_cost_search(problem_instance)
+
+print("Solution:", solution)
