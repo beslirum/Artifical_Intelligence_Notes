d9faa1dcd0946d9a35b64a8975ed8604041384a3
Rümeysa B <102685123+beslirum@users.noreply.github.com>
2023-12-11 13:39:02 +0300

    Update uniform_cost_search.py

--- uniform_cost_search.py
+++ uniform_cost_search.py
@@ -1,1 +1,16 @@
 
+function UNIFORM-COST-SEARCH(problem) returns a solution, or failure
+node ←a node with STATE = problem.INITIAL-STATE, PATH-COST = 0
+frontier ← a priority queue ordered by PATH-COST, with node as the only element
+explored ← an empty set
+loop do
+if EMPTY?(frontier ) then return failure
+node ← POP(frontier ) /* chooses the lowest-cost node in frontier */
+if problem.GOAL-TEST(node.STATE) then return SOLUTION(node)
+add node.STATE to explored
+for each action in problem.ACTIONS(node.STATE) do
+child ← CHILD-NODE(problem, node, action)
+if child.STATE is not in explored or frontier then
+frontier ← INSERT(child,frontier )
+else if child.STATE is in frontier with higher PATH-COST then
+replace that frontier node with child
