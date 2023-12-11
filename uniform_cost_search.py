6b22deca849174a6d03783383cdfc8ebe56a80e4
Rümeysa B <102685123+beslirum@users.noreply.github.com>
2023-12-11 13:49:22 +0300

    Update uniform_cost_search.py

--- uniform_cost_search.py
+++ uniform_cost_search.py
@@ -23,20 +23,27 @@ function UNIFORM-COST-SEARCH(problem) returns a solution, or failure
         node ← POP(frontier)
         
         # Check if the goal state is reached, return the solution if true
+        #Hedef duruma ulaşılıp ulaşılmadığını kontrol edin, doğruysa çözümü döndürün
         if problem.GOAL-TEST(node.STATE) then return SOLUTION(node)
         
         # Add the current node's state to the set of explored states
+        #Geçerli düğümün durumunu explored states kümesine ekleyin
         add node.STATE to explored
         
         # Expand the current node by generating child nodes for each possible action
+        #Her olası eylem için alt düğümler oluşturarak mevcut düğümü genişletin
         for each action in problem.ACTIONS(node.STATE) do
             # Generate a child node based on the current action
+            #Geçerli eyleme göre bir alt düğüm oluştur
             child ← CHILD-NODE(problem, node, action)
             
             # Check if the child state is not in explored or frontier
+            #child state'in explored veya frontierde olup olmadığını kontrol et
             if child.STATE is not in explored or frontier then
                 # Add the child to the frontier
+                #child node'u frontier node'a ekle
                 frontier ← INSERT(child, frontier)
             else if child.STATE is in frontier with higher PATH-COST then
                 # Replace the frontier node with the child if it has a lower path cost
+                #Daha düşük bir yol maliyetine sahipse frontier düğümünü alt düğümle değiştirin
                 replace that frontier node with child
