a52eb9209e023848c4daa23396a18d0c236b18fc
Rümeysa B <102685123+beslirum@users.noreply.github.com>
2023-12-12 11:44:40 +0300

    Update depth_first_search.py

--- depth_first_search.py
+++ depth_first_search.py
@@ -1,8 +1,11 @@
-
 def dfs(vertex, visited):
+    # Eğer düğüm ziyaret edilmemişse
     if vertex not in visited:
+        # Düğümü ziyaret edildi olarak işaretle
         visited.add(vertex)
+        # Düğümü işle (örneğin, yazdır veya özel bir işlem yap)
         process(vertex)
+        # Düğümün komşularını ziyaret etmek için DFS'yi çağır
         for neighbor in get_neighbors(vertex):
             dfs(neighbor, visited)
 
@@ -17,9 +20,12 @@ def process(vertex):
     pass
 
 def main():
+    # Ziyaret edilen düğümleri takip etmek için bir küme oluştur
     visited = set()
+    # Tüm düğümleri başlatma noktası olarak alarak DFS'yi başlat
     for vertex in get_all_vertices():
         dfs(vertex, visited)
 
 if __name__ == "__main__":
+    # Ana fonksiyonu çağır
     main()
