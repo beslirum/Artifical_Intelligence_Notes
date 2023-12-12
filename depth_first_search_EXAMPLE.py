b25939ee2ac6d8663972e8d51347934a2fafc7cc
Rümeysa B <102685123+beslirum@users.noreply.github.com>
2023-12-12 11:46:15 +0300

    Update depth_first_search_EXAMPLE.py

--- depth_first_search_EXAMPLE.py
+++ depth_first_search_EXAMPLE.py
@@ -1,18 +1,21 @@
-
-# Basit bir graf sınıfı oluşturuyoruz
 class Graph:
     def __init__(self):
+        # Grafiği temsil etmek için bir sözlük oluşturuyoruz
         self.graph = {}
 
     def add_edge(self, vertex, neighbor):
+        # Bir kenar ekliyoruz; eğer düğüm henüz yoksa oluşturuyoruz
         if vertex not in self.graph:
             self.graph[vertex] = []
         self.graph[vertex].append(neighbor)
 
 def dfs(graph, start, visited):
+    # Eğer düğüm ziyaret edilmemişse
     if start not in visited:
+        # Düğümü ziyaret edildi olarak işaretle
         print("Visiting:", start)
         visited.add(start)
+        # Düğümün komşularını ziyaret etmek için DFS'yi çağır
         for neighbor in graph.get(start, []):
             dfs(graph, neighbor, visited)
 
